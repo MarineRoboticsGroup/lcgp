5f25f657fd6edb1157bec1571425b107c3955b71
alanpapalia <alanpapalia@gmail.com>
2020-08-22 08:24:48 -0400

    updated graph function naming

--- graph.py
+++ graph.py
@@ -8,7 +8,7 @@ from scipy.linalg import null_space, toeplitz
 import math_utils
 import environment
 import kdtree
-from snl import SolveSNLWithSDP
+from snl import solve_snl_with_sdp
 
 class Graph:
     def __init__(self, noise_model:str, noise_stddev:float):
@@ -23,18 +23,18 @@ class Graph:
         self.noise_stddev = noise_stddev
         self.fisher_info_matrix = None
 
-    def PerformSNL(self, init_guess, solver:str=None):
+    def perform_snl(self, init_guess, solver:str=None):
         num_anchors = 3
-        num_nodes = self.getNumNodes() - num_anchors
+        num_nodes = self.get_num_nodes() - num_anchors
         anchor_ids = [v+num_nodes for v in range(num_anchors)]
         anchor_locs = {}
         for id in anchor_ids:
-            anchor_locs[id] = self.getNodePositionTuple(id)
+            anchor_locs[id] = self.get_node_loc_tuple(id)
         node_node_dists = {}
         node_anchor_dists = {}
-        for edge in self.getGraphEdgeList():
+        for edge in self.get_graph_edge_list():
             i, j = edge
-            dist = self.getEdgeDistScal(edge)
+            dist = self.get_edge_dist_scal(edge)
             if self.noise_model == "add":
                 noise = np.random.normal(0, self.noise_stddev)
                 noisy_dist = dist+noise
@@ -51,223 +51,223 @@ class Graph:
             else:
                 node_node_dists[edge] = noisy_dist
 
-        loc_est = SolveSNLWithSDP(num_nodes, node_node_dists, node_anchor_dists, anchor_locs, anchor_ids, init_guess=init_guess, use_spring_solver=use_spring_solver)
+        loc_est = solve_snl_with_sdp(num_nodes, node_node_dists, node_anchor_dists, anchor_locs, anchor_ids, init_guess=init_guess, solver=solver)
         return loc_est
 
     ###### Initialize and Format Graph ########
-    def initializeFromLocationList(self, locationList, radius):
-        self.removeAllNodes()
+    def initialize_from_location_list(self, locationList, radius):
+        self.remove_all_nodes()
         for loc in locationList:
-            self.addNode(loc[0], loc[1])
-        self.updateEdgesByRadius(radius)
+            self.add_node(loc[0], loc[1])
+        self.update_edges_by_radius(radius)
 
-    def addNode(self, xLoc, yLoc):
+    def add_node(self, xLoc, yLoc):
         self.nodes.append(Node(xLoc, yLoc))
         self.nNodes += 1
 
-    def addGraphEdge(self, node1, node2):
-        assert(self.nodeExists(node1))
-        assert(self.nodeExists(node2))
+    def add_graph_edge(self, node1, node2):
+        assert(self.node_exists(node1))
+        assert(self.node_exists(node2))
 
         edge = (node1, node2)
-        if not (self.edgeExists(edge)):
+        if not (self.edge_exists(edge)):
             self.edges.append(edge)
-            self.edgeDistances.append(self.getEdgeDistScal(edge))
+            self.edgeDistances.append(self.get_edge_dist_scal(edge))
             self.nEdges += 1
-            self.nodes[node1].addNodeEdge(node2)
-            self.nodes[node2].addNodeEdge(node1)
+            self.nodes[node1].add_node_edge(node2)
+            self.nodes[node2].add_node_edge(node1)
 
-    def removeGraphNode(self, nodeNum):
-        assert(self.nodeExists(nodeNum))
-        self.removeConnectingNodeEdges(nodeNum)
+    def remove_graph_node(self, nodeNum):
+        assert(self.node_exists(nodeNum))
+        self.remove_connecting_node_edges(nodeNum)
         self.nodes.remove(nodeNum)
         self.nNodes -= 1
 
-    def removeGraphEdge(self, edge):
-        assert(self.edgeExists(edge))
+    def remove_graph_edge(self, edge):
+        assert(self.edge_exists(edge))
         self.nEdges -= 1
         n1, n2 = edge
         if (n1,n2) in self.edges:
             self.edges.remove((n1, n2))
         else:
             self.edges.remove((n2, n1))
-        self.nodes[n1].removeNodeEdge(n2)
-        self.nodes[n2].removeNodeEdge(n1)
+        self.nodes[n1].remove_node_edge(n2)
+        self.nodes[n2].remove_node_edge(n1)
 
-    def removeAllNodes(self, ):
-        self.removeAllEdges()
+    def remove_all_nodes(self, ):
+        self.remove_all_edges()
         self.nodes.clear()
         self.nNodes = 0
 
-    def removeAllEdges(self, ):
+    def remove_all_edges(self, ):
         self.nEdges = 0
         self.edges.clear()
         self.edgeDistances.clear()
 
-    def updateEdgesByRadius(self, radius):
-        self.removeAllEdges()
+    def update_edges_by_radius(self, radius):
+        self.remove_all_edges()
         if self.nNodes <= 1:
             return
-        nodeKDTree = kdtree.KDTree(self.getNodeLocationList())
+        nodeKDTree = kdtree.KDTree(self.get_node_loc_list())
         self.nEdges = 0
         for r1 in range(self.nNodes):
-            curLoc = self.nodes[r1].getXYLocation()
+            curLoc = self.nodes[r1].get_loc_tuple()
             index, dists = nodeKDTree.search(np.array(curLoc).reshape(2, 1), k=self.nNodes)
             inds = index[0][1:]
             ds = dists[0][1:]
             for r2, d2 in zip(inds, ds):
                 if d2 < radius:
-                    self.addGraphEdge(r1, r2)
+                    self.add_graph_edge(r1, r2)
 
-    def removeConnectingNodeEdges(self, nodeNum):
-        assert(self.nodeExists(nodeNum))
-        edgeList = self.getListOfNodeEdgePairs(nodeNum).copy()
+    def remove_connecting_node_edges(self, nodeNum):
+        assert(self.node_exists(nodeNum))
+        edgeList = self.get_list_of_node_edge_pairs(nodeNum).copy()
         for connection in edgeList:
-            self.removeGraphEdge(connection)
+            self.remove_graph_edge(connection)
             self.nEdges -= 1
 
     ###### Graph Accessors ########
-    def getGraphEdgeList(self, ):
+    def get_graph_edge_list(self, ):
         return self.edges
 
-    def getGraphNodeList(self, ):
+    def get_graph_node_list(self, ):
         return self.nodes
 
-    def getNumNodes(self, ):
+    def get_num_nodes(self, ):
         return self.nNodes
 
-    def getNumEdges(self, ):
+    def get_num_edges(self, ):
         return self.nEdges
 
-    def getNthEigval(self, n):
-        eigvals = math_utils.getListOfAllEigvals(self.getStiffnessMatrix())
+    def get_nth_eigval(self, n):
+        eigvals = math_utils.getListOfAllEigvals(self.get_fisher_matrix())
         eigvals.sort()
         return eigvals[n-1]
 
-    def getStiffnessMatrix(self, ):
+    def get_fisher_matrix(self, ):
         n = self.nNodes
         K = np.zeros((2*n, 2*n))
         return math_utils.buildStiffnessMatrix(self.edges, self.nodes, self.noise_model, self.noise_stddev)
 
-    def getNodeLocationList(self, ):
+    def get_node_loc_list(self, ):
         locs = []
         for node in self.nodes:
-            locs.append(node.getXYLocation())
+            locs.append(node.get_loc_tuple())
         return locs
 
     ###### Node Accessors ########
-    def getNodePositionTuple(self, nodeNum):
-        assert (self.nodeExists(nodeNum))
+    def get_node_loc_tuple(self, nodeNum):
+        assert (self.node_exists(nodeNum))
         node = self.nodes[nodeNum]
-        return node.getXYLocation()
+        return node.get_loc_tuple()
 
-    def getNodeDegree(self, nodeNum):
-        assert (nodeExists(n))
+    def get_node_degree(self, nodeNum):
+        assert (node_exists(n))
         node = self.nodes[nodeNum]
-        return node.getNodeDegree()
+        return node.get_node_degree()
 
-    def getNodeConnectionList(self, nodeNum):
+    def get_node_connection_list(self, nodeNum):
         node = self.nodes[nodeNum]
-        return node.getNodeConnections()
+        return node.get_node_connections()
 
-    def getListOfNodeEdgePairs(self, nodeNum):
-        assert(self.nodeExists(nodeNum))
+    def get_list_of_node_edge_pairs(self, nodeNum):
+        assert(self.node_exists(nodeNum))
         node = self.nodes[nodeNum]
         edges = []
-        for node2 in node.getNodeConnections():
+        for node2 in node.get_node_connections():
             edge = (nodeNum, node2)
             edges.append(edge)
         return edges
 
-    def getEdgeDistVec(self, edge):
-        assert(self.edgeExists(edge))
+    def get_edge_dist_vec(self, edge):
+        assert(self.edge_exists(edge))
         id1, id2 = edge
         node1 = self.nodes[id1]
         node2 = self.nodes[id2]
-        x1, y1 = node1.getXYLocation()
-        x2, y2 = node2.getXYLocation()
+        x1, y1 = node1.get_loc_tuple()
+        x2, y2 = node2.get_loc_tuple()
         v = np.array([x1-x2, y1-y2])
         return v
 
-    def getEdgeDistScal(self, edge):
-        assert(self.edgeExists(edge))
-        v = self.getEdgeDistVec(edge)
+    def get_edge_dist_scal(self, edge):
+        assert(self.edge_exists(edge))
+        v = self.get_edge_dist_vec(edge)
         return la.norm(v, 2)
 
-    def getDistVecBetweenNodes(self, n1 , n2):
-        assert(self.nodeExists(n1))
-        assert(self.nodeExists(n2))
+    def get_dist_vec_between_nodes(self, n1 , n2):
+        assert(self.node_exists(n1))
+        assert(self.node_exists(n2))
         node1 = self.nodes[n1]
         node2 = self.nodes[n2]
-        x1, y1 = node1.getXYLocation()
-        x2, y2 = node2.getXYLocation()
+        x1, y1 = node1.get_loc_tuple()
+        x2, y2 = node2.get_loc_tuple()
         v = np.array([x1-x2, y1-y2])
         return v
 
-    def getDistBetweenNodes(self, n1 , n2):
-        assert(self.nodeExists(n1))
-        assert(self.nodeExists(n2))
-        v = self.getDistVecBetweenNodes(n1, n2)
+    def get_dist_scal_between_nodes(self, n1 , n2):
+        assert(self.node_exists(n1))
+        assert(self.node_exists(n2))
+        v = self.get_dist_vec_between_nodes(n1, n2)
         dist = la.norm(v, 2)
         return dist
 
     ####### Construct Graph Formations #######
-    def initializeTest6(self):
-        self.addNode(0, 0)
-        self.addNode(0, 2)
-        self.addNode(1, 3)
-        self.addNode(2, 2)
-        self.addNode(2, 0)
-        self.addNode(1, 1)
+    def init_test6_formation(self):
+        self.add_node(0, 0)
+        self.add_node(0, 2)
+        self.add_node(1, 3)
+        self.add_node(2, 2)
+        self.add_node(2, 0)
+        self.add_node(1, 1)
 
-    def initializeTest8(self):
-        self.addNode(2, 2)
-        self.addNode(2, 4)
-        self.addNode(4, 2)
-        self.addNode(4, 4)
-        self.addNode(6, 4)
-        self.addNode(6, 6)
-        self.addNode(8, 6)
-        self.addNode(8, 8)
+    def init_test8_formation(self):
+        self.add_node(2, 2)
+        self.add_node(2, 4)
+        self.add_node(4, 2)
+        self.add_node(4, 4)
+        self.add_node(6, 4)
+        self.add_node(6, 6)
+        self.add_node(8, 6)
+        self.add_node(8, 8)
 
-    def initializeSquare(self):
-        # self.addNode(1, 1)
-        # self.addNode(1, 2)
-        # self.addNode(2, 2)
-        # self.addNode(2, 1)
-        self.addNode(2, 2)
-        self.addNode(2, 6)
-        self.addNode(6, 6)
-        self.addNode(6, 2)
+    def init_square_formation(self):
+        # self.add_node(1, 1)
+        # self.add_node(1, 2)
+        # self.add_node(2, 2)
+        # self.add_node(2, 1)
+        self.add_node(2, 2)
+        self.add_node(2, 6)
+        self.add_node(6, 6)
+        self.add_node(6, 2)
 
-    def initializeRandomConfig(self, numRobots, bounds):
+    def init_random_formation(self, numRobots, bounds):
         xbound, ybound = bounds
         xVal = np.random.uniform(low=1, high=10, size=numRobots)
         yVal = np.random.uniform(low=1, high=10, size=numRobots)
         for i in range(numRobots):
 
-            self.addNode(xVal[i], yVal[i])
+            self.add_node(xVal[i], yVal[i])
 
     ####### Controls #######
-    def moveTowardsVec(self, vec, relativeMovement=True):
+    def move_to(self, vec, relativeMovement=True):
         assert(len(vec) == 2*len(self.nodes))
         for i, node in enumerate(self.nodes):
             newX = vec[2*i]
             newY = vec[2*i+1]
 
             if relativeMovement:
-                node.moveNodeRelative(newX, newY)
+                node.move_node_relative(newX, newY)
             else:
-                node.moveNodeAbsolute(newX, newY)
+                node.move_node_absolute(newX, newY)
 
     ####### Testing #######
-    def edgeExists(self, edge):
+    def edge_exists(self, edge):
         n1, n2 = edge[0], edge[1]
-        assert(self.nodeExists(n1))
-        assert(self.nodeExists(n2))
+        assert(self.node_exists(n1))
+        assert(self.node_exists(n2))
         return (((n1, n2) in self.edges ) or ((n2, n1) in self.edges ))
 
-    def nodeExists(self, node):
+    def node_exists(self, node):
         return (node < self.nNodes)
 
 class Node:
@@ -277,27 +277,27 @@ class Node:
         self._degree = 0
         self._connections = []
 
-    def getNodeConnections(self, ):
+    def get_node_connections(self, ):
         return self._connections
 
-    def getNodeDegree(self, ):
+    def get_node_degree(self, ):
         return self._degree
 
-    def getXYLocation(self, ):
+    def get_loc_tuple(self, ):
         return (self._x, self._y)
 
-    def addNodeEdge(self, edgeNodeNum):
+    def add_node_edge(self, edgeNodeNum):
         self._degree += 1
         self._connections.append(edgeNodeNum)
 
-    def removeNodeEdge(self, edgeNodeNum):
+    def remove_node_edge(self, edgeNodeNum):
         self._degree -= 1
         self._connections.remove(edgeNodeNum)
 
-    def moveNodeAbsolute(self, newX, newY):
+    def move_node_absolute(self, newX, newY):
         self._x = newX
         self._y = newY
 
-    def moveNodeRelative(self, deltaX, deltaY):
+    def move_node_relative(self, deltaX, deltaY):
         self._x += deltaX
         self._y += deltaY
