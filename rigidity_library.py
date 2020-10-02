0f5ca26ddbb5584842a075a1218a05a710a77883
alanpapalia <alanpapalia@gmail.com>
2020-10-02 11:20:42 -0400

    updated rigidity library construction to be multiprocessed

--- rigidity_library.py
+++ rigidity_library.py
@@ -1,6 +1,7 @@
 import math_utils
 import graph
 
+import multiprocessing
 from typing import Tuple, List
 import itertools
 import tracemalloc
@@ -10,6 +11,90 @@ import time
 import os
 import json
 
+def _generate_rigidity_dict(rigidity_library, num_robots, dist_between_nodes, noise_model, noise_stddev, sensing_radius, num_rows, num_cols, all_possible_node_indices):
+    def check_if_locs_aligned(node_indices) -> bool:
+        """This function is to check if the nodes are properly aligned in
+        the checkerboard frame, as many of the possible combinations will
+        not be. This is because we intend to first align such that there is
+        a robot on the 0th row and a robot on the 0th column of the
+        checkerboard
+
+        Args:
+            node_indices (Tuple[Tuple]): Tuple of location tuples
+
+        Returns:
+            bool: whether or not the locations were aligned and thus valid
+                to add to the dictionary
+        """
+        if not (0 == node_indices[0][0]):
+            return False
+        for loc in node_indices:
+            if loc[1] == 0:
+                return True
+        return False
+
+    def convert_indices_to_locs(node_indices):
+        # Note: the x and y axes don't have to correspond with the
+        # actual coordinates seen of a graph as long as there is
+        # consistency in the labeling and the distances in x and y
+        # directions are equal
+
+        node_locs = []
+        for indices in node_indices:
+            x_pos = indices[0] * dist_between_nodes
+            y_pos = indices[1] * dist_between_nodes
+            pos = (x_pos, y_pos)
+            node_locs.append(pos)
+        return node_locs
+
+    def write_locs_to_dict(combo, d):
+        """Takes an iterable item containing possible locations on the checkerboard, computes corresponding rigidity values for these items, and then stores these values in a given dict
+
+        Args:
+            combo (iterable[Tuple]): iterable container of tuple of location
+                combinations on checkerboard
+            d (dict): relates each tuple of locations to a given rigidity
+                value
+        """
+        test_graph = graph.Graph(noise_model, noise_stddev)
+        for j, node_indices in enumerate(combo):
+
+            # check if location is aligned on row 0 and col 0
+            if not check_if_locs_aligned(node_indices):
+                continue
+
+            # convert to XY style coordinates and then test the rigidity of
+            # the corresponding graph
+            node_locs = convert_indices_to_locs(node_indices)
+            test_graph.initialize_from_location_list(node_locs, sensing_radius)
+            rigidity_val = test_graph.get_nth_eigval(4)
+            d[str(node_indices)] = rigidity_val
+
+            if j % 2000 == 0 and False:
+                print(f"Len Dict: {len(d)}")
+                current, peak = tracemalloc.get_traced_memory()
+                if True:
+                    print(
+                        f"Current Memory: {current * 1e-9} GB"
+                        + f"\nPeak Memory: {peak *1e-9} GB"
+                        + f"\nDict Size : {getsizeof(d) * 1e-9} GB"
+                        + f"\nNode Locs Size : {getsizeof(combo) * 1e-9} GB"
+                        + "\n"
+                    )
+
+                assert (
+                    current * 1e-9 < 3
+                ), f"Overused memory. Stopping process. Current memory usage {current*1e-9} GB"
+
+    print(
+        f"Building Rigidity Library: {num_robots} robots, {dist_between_nodes} spacing, {num_rows} rows, {num_cols} cols"
+    )
+    rigidity_dict = {}
+    possible_node_locs = itertools.combinations(
+        all_possible_node_indices, num_robots
+    )
+    write_locs_to_dict(possible_node_locs, rigidity_dict)
+    rigidity_library[num_robots] = rigidity_dict
 
 class RigidityLibrary:
     """This class is basically holding a bunch of dictionaries of precomputed
@@ -110,91 +195,6 @@ class RigidityLibrary:
         with open(self.filepath, "w") as f:
             json.dump(self.rigidity_library, f)
 
-    def _generate_rigidity_dict(self, num_robots):
-        def check_if_locs_aligned(node_indices) -> bool:
-            """This function is to check if the nodes are properly aligned in
-            the checkerboard frame, as many of the possible combinations will
-            not be. This is because we intend to first align such that there is
-            a robot on the 0th row and a robot on the 0th column of the
-            checkerboard
-
-            Args:
-                node_indices (Tuple[Tuple]): Tuple of location tuples
-
-            Returns:
-                bool: whether or not the locations were aligned and thus valid
-                    to add to the dictionary
-            """
-            if not (0 == node_indices[0][0]):
-                return False
-            for loc in node_indices:
-                if loc[1] == 0:
-                    return True
-            return False
-
-        def convert_indices_to_locs(node_indices):
-            # Note: the x and y axes don't have to correspond with the
-            # actual coordinates seen of a graph as long as there is
-            # consistency in the labeling and the distances in x and y
-            # directions are equal
-
-            node_locs = []
-            for indices in node_indices:
-                x_pos = indices[0] * self.dist_between_nodes
-                y_pos = indices[1] * self.dist_between_nodes
-                pos = (x_pos, y_pos)
-                node_locs.append(pos)
-            return node_locs
-
-        def write_locs_to_dict(combo, d):
-            """Takes an iterable item containing possible locations on the checkerboard, computes corresponding rigidity values for these items, and then stores these values in a given dict
-
-            Args:
-                combo (iterable[Tuple]): iterable container of tuple of location
-                    combinations on checkerboard
-                d (dict): relates each tuple of locations to a given rigidity
-                    value
-            """
-            test_graph = graph.Graph(self.noise_model, self.noise_stddev)
-            for j, node_indices in enumerate(combo):
-
-                # check if location is aligned on row 0 and col 0
-                if not check_if_locs_aligned(node_indices):
-                    continue
-
-                # convert to XY style coordinates and then test the rigidity of
-                # the corresponding graph
-                node_locs = convert_indices_to_locs(node_indices)
-                test_graph.initialize_from_location_list(node_locs, self.sensing_radius)
-                rigidity_val = test_graph.get_nth_eigval(4)
-                d[str(node_indices)] = rigidity_val
-
-                if j % 2000 == 0 and False:
-                    print(f"Len Dict: {len(d)}")
-                    current, peak = tracemalloc.get_traced_memory()
-                    if True:
-                        print(
-                            f"Current Memory: {current * 1e-9} GB"
-                            + f"\nPeak Memory: {peak *1e-9} GB"
-                            + f"\nDict Size : {getsizeof(d) * 1e-9} GB"
-                            + f"\nNode Locs Size : {getsizeof(combo) * 1e-9} GB"
-                            + "\n"
-                        )
-
-                    assert (
-                        current * 1e-9 < 3
-                    ), f"Overused memory. Stopping process. Current memory usage {current*1e-9} GB"
-
-        print(
-            f"Building Rigidity Library: {num_robots} robots, {self.dist_between_nodes} spacing, {self.num_rows} rows, {self.num_cols} cols"
-        )
-        rigidity_dict = {}
-        possible_node_locs = itertools.combinations(
-            self.all_possible_node_indices, num_robots
-        )
-        write_locs_to_dict(possible_node_locs, rigidity_dict)
-        return rigidity_dict
-
     def init_rigidity_library(self):
         """
         Tries to read the rigidity library from a presaved file. If file not
@@ -202,23 +202,58 @@ class RigidityLibrary:
         future use
         """
 
+# def _generate_rigidity_dict(num_robots, dist_between_nodes, noise_model, noise_stddev, sensing_radius, num_rows, num_cols, all_possible_node_indices):
+
         self.rigidity_library = self.read_rigidity_library()
 
         # if no rigidity library read, construct one
         if self.rigidity_library is None:
-            self.rigidity_library = {}
             print(f"No rigidity library read from file. Constructing one now")
-            for num_robot in range(3, self.max_num_robots + 1):
-                self.rigidity_library[num_robot] = self._generate_rigidity_dict(
-                    num_robot
-                )
 
-        cur_num_robots = self.get_current_num_robots()
-        if cur_num_robots < self.max_num_robots:
-            for num_robots in range(cur_num_robots, self.max_num_robots + 1):
-                self.rigidity_library[num_robot] = self._generate_rigidity_dict(
-                    num_robot
-                )
+            manager = multiprocessing.Manager()
+            temp_rigidity_library = manager.dict()
+            nproc = multiprocessing.cpu_count()
+            pool = multiprocessing.Pool(nproc-2)
+            for num_robots in range(3, self.max_num_robots + 1):
+                #* synchronous
+                # _generate_rigidity_dict(temp_rigidity_library, num_robots, self.dist_between_nodes, self.noise_model, self.noise_stddev, self.sensing_radius, self.num_rows, self.num_cols, self.all_possible_node_indices)
+
+                # *multiprocessed
+                pool.apply_async(_generate_rigidity_dict, args = (temp_rigidity_library, num_robots, self.dist_between_nodes, self.noise_model, self.noise_stddev, self.sensing_radius, self.num_rows, self.num_cols, self.all_possible_node_indices))
+
+            pool.close()
+            pool.join()
+            self.rigidity_library = dict(temp_rigidity_library)
+        else:
+            cur_num_robots = self.get_current_num_robots()
+            if cur_num_robots < self.max_num_robots:
+                manager = multiprocessing.Manager()
+                temp_rigidity_library = manager.dict(self.rigidity_library)
+
+                nproc = multiprocessing.cpu_count()
+                pool = multiprocessing.Pool(nproc-2)
+                for num_robots in range(cur_num_robots, self.max_num_robots + 1):
+                    #* synchronous
+                    # _generate_rigidity_dict(temp_rigidity_library, num_robots, self.dist_between_nodes, self.noise_model, self.noise_stddev, self.sensing_radius, self.num_rows, self.num_cols, self.all_possible_node_indices)
+
+                    # *multiprocessed
+                    pool.apply_async(_generate_rigidity_dict, args = (temp_rigidity_library, num_robots, self.dist_between_nodes, self.noise_model, self.noise_stddev, self.sensing_radius, self.num_rows, self.num_cols, self.all_possible_node_indices))
+                pool.close()
+                pool.join()
+                self.rigidity_library = dict(temp_rigidity_library)
 
         # write library to file so can be read in the future
         self.write_rigidity_library()
+
+if __name__ == "__main__":
+    """ Just a little testing of functionality 
+    """
+    dist_between_nodes = 1
+    sensing_radius = 6
+    noise_stddev= 0.65
+    noise_model= 'add'
+    num_rows= 4
+    num_cols= 4
+    max_num_robots= 8
+
+    r = RigidityLibrary(dist_between_nodes, sensing_radius, noise_stddev, noise_model, num_rows, num_cols, max_num_robots)
