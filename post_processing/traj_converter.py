0cd26c664f718a8fcc473487756e7cae5daa2339
alanpapalia <alanpapalia@gmail.com>
2021-02-25 15:14:24 -0500

    modified traj converter output format

--- post_processing/traj_converter.py
+++ post_processing/traj_converter.py
@@ -3,10 +3,11 @@ def write_grouped_by_robot(input_file: str, output_file: str):
     f1 = open(input_file, "r")
     all_trajs = f1.readlines()
 
-
     # reformat individual trajectories
     max_traj_length = 0
     output = []
+    x_trajs = []
+    y_trajs = []
     for traj in all_trajs:
         # get relevant data from input
         for char in "[()] ":
@@ -25,35 +26,58 @@ def write_grouped_by_robot(input_file: str, output_file: str):
             x_waypoints.append(x)
             y_waypoints.append(y)
 
+        x_trajs.append(x_waypoints)
+        y_trajs.append(y_waypoints)
         output.extend([x_waypoints, "\n", y_waypoints, "\n\n"])
-        max_traj_length = max(max_traj_length, len(x_waypoints))
+        # max_traj_length = max(max_traj_length, len(x_waypoints))
 
     # make sure each trajectory is the same length
     for i in range(len(output)):
         if type(output[i]) != list:  # filter out newlines
             continue
         traj = output[i]
-        print(traj)
-        print(traj[-1])
-        print([traj[-1] for i in range(max_traj_length - len(traj))])
+        # print(traj)
+        # print(traj[-1])
+        # print([traj[-1] for i in range(max_traj_length - len(traj))])
         output[i] = traj + [traj[-1] for i in range(max_traj_length - len(traj))]
 
+    traj_len = len(x_trajs)
+    assert traj_len == len(y_trajs)
+    with open(output_file, "w") as file:
+        for robot_id in range(traj_len):
+            file.write("Robot %d \n" % (robot_id+1))
+            x_traj = str(x_trajs[robot_id])  # convert to string
+            x_traj = x_traj[1:-1]  # get rid of brackets
+            x_traj = x_traj.replace(",", "")  # trim commas
+            file.write("[%s]\n" % str(x_traj)[1:-1])
 
-    # add list with as many zeros as waypoints
-    output.append([0 for i in range(len(output[0]))])
+            y_traj = str(y_trajs[robot_id])  # convert to string
+            y_traj = y_traj[1:-1]  # get rid of brackets
+            y_traj = y_traj.replace(",", "")  # trim commas
+            file.write("[%s]" % (y_traj))
+            file.write("\n\n")
+            print("printed robot %d" % robot_id)
 
+        # file.write("Waypoint Y Coordinates\n")
+        # file.write("{")
+        # for i, traj in enumerate(y_trajs):
+        #     if i == traj_len-1:
+        #     file.write("{%s} " % str(traj)[1:-1])
+        # else:
+        #         file.write("{%s}, " % str(traj)[1:-1])
+        # file.write("}; \n\n")
 
     # saved newly formatted trajectories
-    output = [str(output[i]) for i in range(len(output))]
-    f2 = open(output_file, "w")
-    f2.writelines(output)
-    f2.close()
+    # output = [str(output[i]) for i in range(len(output))]
+    # f2 = open(output_file, "w")
+    # f2.writelines(output)
+    # f2.close()
+
 
 def write_grouped_by_coordinates(input_file: str, output_file: str):
     # load generated trajectories
     f1 = open(input_file, "r")
     all_trajs = f1.readlines()
-
 
     # reformat individual trajectories
     max_traj_length = 0
@@ -93,11 +117,11 @@ def write_grouped_by_coordinates(input_file: str, output_file: str):
 
     traj_len = len(x_trajs)
     assert traj_len == len(y_trajs)
-    with open(output_file, 'w') as file:
+    with open(output_file, "w") as file:
         file.write("Waypoint X Coordinates\n")
         file.write("{")
         for i, traj in enumerate(x_trajs):
-            if i == traj_len-1:
+            if i == traj_len - 1:
                 file.write("{%s} " % str(traj)[1:-1])
             else:
                 file.write("{%s}, " % str(traj)[1:-1])
@@ -107,7 +131,7 @@ def write_grouped_by_coordinates(input_file: str, output_file: str):
         file.write("Waypoint Y Coordinates\n")
         file.write("{")
         for i, traj in enumerate(y_trajs):
-            if i == traj_len-1:
+            if i == traj_len - 1:
                 file.write("{%s} " % str(traj)[1:-1])
             else:
                 file.write("{%s}, " % str(traj)[1:-1])
@@ -119,18 +143,19 @@ def write_grouped_by_coordinates(input_file: str, output_file: str):
     # f2.close()
 
 
-
 if __name__ == "__main__":
     input_file = "input_trajs.txt"
     output_file = "output_trajs.txt"
 
-    grouping = "robots" # write in groupings by robot trajectories
-    grouping = "coordinates" # write grouped by coordinates
+    grouping = "robots"  # write in groupings by robot trajectories
+    # grouping = "coordinates" # write grouped by coordinates
 
     if grouping == "robots":
+        print("writing grouped by robot ids")
         write_grouped_by_robot(input_file, output_file)
 
     elif grouping == "coordinates":
+        print("writing grouped by coords")
         write_grouped_by_coordinates(input_file, output_file)
 
     else:
